ee11fce1b5b755f8c240cff6943efa8a327bc867
Your Name <you@example.com>
2024-09-22 00:26:28 +0530

    commit

---
+++ db_connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$server_name = "localhost";
+$user_name = "root";
+$user_pass = '';
+$database_name = "music_user";
+
+$con = mysqli_connect($server_name, $user_name, $user_pass, $database_name);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_error());
+}
+//  else {
+//     echo('Database Successfully Connected');
+// }
+
+?>
